18bf871f32e07a2ff3318f178263249d733af3b5
littleBlackMan <littleblackman@users.noreply.github.com>
2018-04-05 15:10:03 +0200

    Update index.php
    if the controller is an object, you should call the object

--- index.php
+++ index.php
@@ -3,11 +3,15 @@ require_once('Controller/controller.php');
 
 if (isset($_GET['action'])) {
 	if ($_GET['action'] == 'listPosts') {
-		listPosts();
+		$controller = new Controller();
+		$controller->listPosts()
+		//listPosts();
 	}
 	elseif ($_GET['action'] == 'chapter') {
 		if (isset($_GET['id']) && $_GET['id'] >0) {
-			chapter();
+			$controller = new Controller();
+			$controller->chapter();
+			//chapter();
 		}
 		else {
 			echo 'Erreur : aucun identifiant de billet envoyé';
